f407544491f29fad71a07852604c3f4869db793c
anubhav1941 <anubhav1941@gmail.com>
2026-04-06 17:13:01 +0000

    fix: terraform cleanup before pull

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,20 +1,13 @@
 module "eks" {
-<<<<<<< HEAD
-=======
-
->>>>>>> 562b6fa6b5687c5f2d6bb3fb36717fb0ee002bdf
   source  = "terraform-aws-modules/eks/aws"
   version = "19.15.1"
 
   cluster_name                   = local.name
   cluster_endpoint_public_access = true
 
-<<<<<<< HEAD
-  create_kms_key = false
+  create_kms_key            = false
   cluster_encryption_config = []
 
-=======
->>>>>>> 562b6fa6b5687c5f2d6bb3fb36717fb0ee002bdf
   cluster_addons = {
     coredns = {
       most_recent = true
@@ -31,36 +24,12 @@ module "eks" {
   subnet_ids               = module.vpc.public_subnets
   control_plane_subnet_ids = module.vpc.intra_subnets
 
-<<<<<<< HEAD
   eks_managed_node_group_defaults = {
-    instance_types = ["t2.large"]
-  }
-
-  eks_managed_node_groups = {
-    default = {
-      desired_size = 2
-      min_size     = 1
-      max_size     = 3
-    }
-  }
-
-  tags = local.tags
-}
-
-=======
-  # EKS Managed Node Group(s)
-
-  eks_managed_node_group_defaults = {
-
-    instance_types = ["t2.large"]
-
+    instance_types                        = ["t2.large"]
     attach_cluster_primary_security_group = true
-
   }
 
-
   eks_managed_node_groups = {
-
     tws-demo-ng = {
       min_size     = 2
       max_size     = 3
@@ -68,21 +37,19 @@ module "eks" {
 
       instance_types = ["t2.large"]
       capacity_type  = "SPOT"
+      disk_size      = 35
 
-      disk_size = 35 
-      use_custom_launch_template = false  # Important to apply disk size!
+      use_custom_launch_template = false
 
       tags = {
-        Name = "tws-demo-ng"
+        Name        = "tws-demo-ng"
         Environment = "dev"
-        ExtraTag = "e-commerce-app"
+        ExtraTag    = "e-commerce-app"
       }
     }
   }
- 
+
   tags = local.tags
-
-
 }
 
 data "aws_instances" "eks_nodes" {
@@ -97,4 +64,3 @@ data "aws_instances" "eks_nodes" {
 
   depends_on = [module.eks]
 }
->>>>>>> 562b6fa6b5687c5f2d6bb3fb36717fb0ee002bdf
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -8,7 +8,6 @@ output "vpc_id" {
   value       = module.vpc.vpc_id
 }
 
-
 output "eks_cluster_name" {
   description = "EKS cluster name"
   value       = module.eks.cluster_name
@@ -19,17 +18,12 @@ output "eks_cluster_endpoint" {
   value       = module.eks.cluster_endpoint
 }
 
-
 output "public_ip" {
   description = "Public IP of the EC2 instance"
   value       = aws_instance.testinstance.public_ip
 }
 
-<<<<<<< HEAD
-
-=======
 output "eks_node_group_public_ips" {
   description = "Public IPs of the EKS node group instances"
   value       = data.aws_instances.eks_nodes.public_ips
 }
->>>>>>> 562b6fa6b5687c5f2d6bb3fb36717fb0ee002bdf
